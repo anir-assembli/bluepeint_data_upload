2255182f19770e455a88b59c98dedb367b64df2a
YashwanthReddyKikkuri <yashwanth@assembli.ai>
2024-09-30 17:58:02 -0500

    Added plan set images and increased file upload limit to 1 GB

--- data-upload-webapp/main.py
+++ data-upload-webapp/main.py
@@ -18,7 +18,7 @@ csrf = CSRFProtect(app)
 
 # Security configurations
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # Replace with a secure key
-app.config['MAX_CONTENT_LENGTH'] = 256 * 1024 * 1024  # 256MB file size limit
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 # 1 GB file size limit
 ALLOWED_EXTENSIONS = {'pdf', 'jpg', 'jpeg', 'png', 'xlsx', 'csv'}
 
 # Logging configuration
